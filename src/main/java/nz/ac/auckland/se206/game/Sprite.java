22e54741678587792e30402e730d04ebf559befc
flynnteh <flynn.teh@gmail.com>
2023-09-10 01:25:19 +1200

    feat - Canvas renderer
    Implemented a canvas renderer class for rendering sprites onto the game canvas. In addition, added more fields for sprite class.

--- src/main/java/nz/ac/auckland/se206/game/Sprite.java
+++ src/main/java/nz/ac/auckland/se206/game/Sprite.java
@@ -6,11 +6,21 @@ public abstract class Sprite {
   protected Image image;
   protected int posX;
   protected int posY;
+  protected int width;
+  protected int height;
 
   public Sprite(String imagePath, int width, int height, int posX, int posY) {
     this.image =
         new Image(
             getClass().getResource(imagePath).toExternalForm(), width, height, false, false, false);
+    this.width = width;
+    this.height = height;
+    this.posX = posX;
+    this.posY = posY;
+  }
+
+  public Image getImage() {
+    return this.image;
   }
 
   public int getPosX() {
@@ -19,6 +29,14 @@ public abstract class Sprite {
 
   public int getPosY() {
     return posY;
+  }
+
+  public int getWidth() {
+    return this.width;
+  }
+
+  public int getHeight() {
+    return this.height;
   }
 
   public void setPosX(int posX) {
