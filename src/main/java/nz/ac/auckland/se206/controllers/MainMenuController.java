b8080f764a9b26e71c3b91b212ddac57241b37a0
Tom <tyal718@aucklanduni.ac.nz>
2023-09-09 16:04:20 +1200

    Made the main menu on SceneBuilder and added basic controller framework

--- src/main/java/nz/ac/auckland/se206/controllers/MainMenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MainMenuController.java
@@ -1,3 +1,28 @@
 package nz.ac.auckland.se206.controllers;
 
-public class MainMenuController {}
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import nz.ac.auckland.se206.App;
+
+public class MainMenuController {
+  @FXML private Button startGameButton;
+  @FXML private Button tutorialButton;
+  @FXML private Button gameSettingButton;
+
+  @FXML
+  private void onSettingsPressed(ActionEvent event) throws IOException {
+    App.setRoot("gamesettings");
+  }
+
+  @FXML
+  private void onTutorialPressed(ActionEvent event) throws IOException {
+    // TODO: implement
+  }
+
+  @FXML
+  private void onStartPressed(ActionEvent event) throws IOException {
+    // TODO: implement
+  }
+}
