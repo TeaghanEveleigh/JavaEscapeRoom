f22f9838b8526424b4c86270db9848e68ab2aaaa
flynnteh <flynn.teh@gmail.com>
2023-09-13 14:06:24 +1200

    feat - Implemented flashing light animation.
    Implemented timeline animation that will flash the current light in the sequence, then move to the next light.

--- src/main/java/nz/ac/auckland/se206/controllers/MemoryGameController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MemoryGameController.java
@@ -1,11 +1,18 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 public class MemoryGameController {
   @FXML private Circle lightOne;
@@ -20,11 +27,13 @@ public class MemoryGameController {
 
   private ArrayList<Circle> lights;
   private HashSet<Circle> sequence;
+  private ArrayList<Circle> lightsPressed;
 
   @FXML
   public void initialize() {
     this.lights = new ArrayList<Circle>();
     this.sequence = new HashSet<Circle>();
+    this.lightsPressed = new ArrayList<Circle>();
 
     lights.add(lightOne);
     lights.add(lightTwo);
@@ -37,6 +46,7 @@ public class MemoryGameController {
     lights.add(lightNine);
 
     ChooseSequence(6);
+    showLights();
   }
 
   private void ChooseSequence(int sequenceLength) {
@@ -49,5 +59,38 @@ public class MemoryGameController {
       sequence.add(chosen);
       chosen.setFill(Color.YELLOW);
     }
+  }
+
+  private void showLights() {
+    Timeline timeline = new Timeline();
+    timeline.setCycleCount(sequence.size());
+    ArrayList<Circle> lightsQueue = new ArrayList<Circle>(sequence);
+    KeyFrame frame =
+        new KeyFrame(
+            Duration.seconds(1),
+            new EventHandler<ActionEvent>() {
+
+              Circle currentLight = null;
+
+              @Override
+              public void handle(ActionEvent event) {
+                if (currentLight != null) {
+                  currentLight.setFill(Color.BLUE);
+                }
+                Circle light = lightsQueue.remove(0);
+                light.setFill(Color.GREEN);
+                currentLight = light;
+              }
+            });
+
+    timeline.getKeyFrames().add(frame);
+    timeline.play();
+  }
+
+  @FXML
+  private void onLightPressed(MouseEvent event) throws IOException {
+    lightsPressed.add((Circle) event.getSource());
+    Circle pressed = (Circle) event.getSource();
+    pressed.setFill(Color.PINK);
   }
 }
