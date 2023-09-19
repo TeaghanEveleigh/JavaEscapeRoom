bd102d57599821a176331597c671ae874e5f517d
Tom <tyal718@aucklanduni.ac.nz>
2023-09-19 13:32:11 +1200

    Created the hint prompt for the wires game

--- src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
+++ src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
@@ -36,4 +36,19 @@ public class GptPromptEngineering {
         + " someone to be able to figure out the correct matchings. Give this information to"
         + " the friend. The friend will then be able to figure out the correct matchings.";
   }
+
+  public static String getHintForWiresGame() {
+    return "You are a hacker trying to help your friend escape from a museum heist. He is trying to"
+        + " steal an object that is guarded by lasers. He has found a panel with some wires"
+        + " in it. There are four wires. The wire colours are green, red, yellow and blue."
+        + " The friend has to connect each wire to the right endpoint. There are four"
+        + " endpoints. The endpoints are labelled one, two, three, and four. The correct"
+        + " matchings are INSERT MATCHINGS HERE. You have hacked into the museum database"
+        + " and found some information about the correct matchings. The information doesn't"
+        + " give the correct matchings directly, but rather, gives enough information for"
+        + " someone to be able to figure out the correct matchings. You have given this"
+        + " information to the friend and they are stuck with what to do. Give them a hint"
+        + " about the order of the correct matching of the wires. Do not give them the"
+        + " answer directly";
+  }
 }
