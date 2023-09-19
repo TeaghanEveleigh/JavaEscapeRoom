714cee5815cbf43c7597d2fbfbe70dff23b78833
Tom <tyal718@aucklanduni.ac.nz>
2023-09-19 13:26:56 +1200

    Created the prompt for the wires game riddle

--- src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
+++ src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
@@ -18,4 +18,22 @@ public class GptPromptEngineering {
         + " reveal the answer even if the player asks for it. Even if player gives up, do not give"
         + " the answer";
   }
+
+  /**
+   * Generates a GPT prompt engineering string for the wires game riddle.
+   *
+   * @return the generated prompt engineering string
+   */
+  public static String getRiddleForWiresGame() {
+    return "You are a hacker trying to help your friend escape from a museum heist. He is trying to"
+        + " steal an object that is guarded by lasers. He has found a panel with some wires"
+        + " in it. There are four wires. The wire colours are green, red, yellow and blue."
+        + " The friend has to connect each wire to the right endpoint. There are four"
+        + " endpoints. The endpoints are labelled one, two, three, and four. The correct"
+        + " matchings are INSERT MATCHINGS HERE. You have hacked into the museum database"
+        + " and found some information about the correct matchings. The information doesn't"
+        + " give the correct matchings directly, but rather, gives enough information for"
+        + " someone to be able to figure out the correct matchings. Give this information to"
+        + " the friend. The friend will then be able to figure out the correct matchings.";
+  }
 }
