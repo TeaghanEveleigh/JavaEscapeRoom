6b56e24b42250077c84e258fe782791279dd6235
flynnteh <flynn.teh@gmail.com>
2023-09-21 22:22:04 +1200

    feat - left dinosaur interaction
    Implemented left dinosaur interactions - still needs to have a real interaction, but label now shows when in proximity.

---
+++ src/main/java/nz/ac/auckland/se206/LeftDinosaurListener.java
@@ -0,0 +1,9 @@
+package nz.ac.auckland.se206;
+
+public interface LeftDinosaurListener {
+  public void leftDinosaurInteracted();
+
+  public void leftDinosaurTouched();
+
+  public void leftDinosaurUntouched();
+}
--- src/main/java/nz/ac/auckland/se206/controllers/LaserRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LaserRoomController.java
@@ -2,14 +2,15 @@ package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Rectangle;
+import nz.ac.auckland.se206.LeftDinosaurListener;
 import nz.ac.auckland.se206.SceneManager.AppUi;
-import nz.ac.auckland.se206.game.Door;
+import nz.ac.auckland.se206.game.LeftDinosaur;
+import nz.ac.auckland.se206.game.Portal;
 import nz.ac.auckland.se206.game.SolidBox;
 
-public class LaserRoomController extends GameController {
+public class LaserRoomController extends GameController implements LeftDinosaurListener {
   @FXML private Label dinoLabel1;
   @FXML private ImageView object;
   @FXML private ImageView laserShadow1;
@@ -22,20 +23,24 @@ public class LaserRoomController extends GameController {
   @FXML private Rectangle boundingBoxTwo;
   @FXML private Rectangle doorRectangle;
   @FXML private Label itemLabel;
+  @FXML private Rectangle leftDinosaurBounds;
+  @FXML private Rectangle rightDinosaurBounds;
+  @FXML private Rectangle objectBounds;
 
   @Override
   public void initialize() {
     super.initialize();
     boundsObjects.add(new SolidBox(boundingBoxOne));
     boundsObjects.add(new SolidBox(boundingBoxTwo));
-    boundsObjects.add(new Door(doorRectangle, AppUi.MAIN_MENU));
+    boundsObjects.add(new Portal(doorRectangle, AppUi.EXIT_ROOM));
+    boundsObjects.add(new LeftDinosaur(leftDinosaurBounds, this));
     this.player.setBoundingBoxes(boundsObjects);
     this.player.setPosX(54);
     this.player.setPosY(472);
-    disableLasers();
-    stealItem();
   }
-  @FXML private void disableLasers(){
+
+  @FXML
+  private void disableLasers() {
     laser1.toBack();
     laser2.toBack();
     laser3.toBack();
@@ -44,22 +49,45 @@ public class LaserRoomController extends GameController {
     laserShadow3.toBack();
     itemLabel.toFront();
   }
-  @FXML private void itemLabelShow(){
-    itemLabel.setOpacity(1);
 
+  @FXML
+  private void itemLabelShow() {
+    itemLabel.setOpacity(1);
   }
-  @FXML private void itemLabelHide(){
+
+  @FXML
+  private void itemLabelHide() {
     itemLabel.setOpacity(0);
   }
-  @FXML private void stealItem(){
-      object.toBack();
-      itemLabel.toBack();
+
+  @FXML
+  private void stealItem() {
+    object.toBack();
+    itemLabel.toBack();
   }
-  @FXML private void showDinoLabel1(){
+
+  @FXML
+  private void showDinoLabel1() {
     dinoLabel1.setOpacity(1);
   }
-  @FXML private void hideDinoLabel1(){
+
+  @FXML
+  private void hideDinoLabel1() {
     dinoLabel1.setOpacity(0);
-    
+  }
+
+  @Override
+  public void leftDinosaurInteracted() {
+    return;
+  }
+
+  @Override
+  public void leftDinosaurTouched() {
+    showDinoLabel1();
+  }
+
+  @Override
+  public void leftDinosaurUntouched() {
+    hideDinoLabel1();
   }
 }
