003522f278e5798fee569fc8aadc5ebfea34a44b
flynnteh <flynn.teh@gmail.com>
2023-10-16 01:20:33 +1300

    feat - timer tasks
    Implemented timer tasks that should increment and decrement based on the suspicion interactable being tocuhed

--- src/main/java/nz/ac/auckland/se206/game/Suspicion.java
+++ src/main/java/nz/ac/auckland/se206/game/Suspicion.java
@@ -1,15 +1,47 @@
 package nz.ac.auckland.se206.game;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.se206.listeners.SuspicionListener;
 
 public class Suspicion extends Interactable {
 
   private SuspicionListener listener;
+  private Timer timer;
+  private TimerTask incrementTask;
+  private TimerTask decrementTask;
+  private TimerTask currentTask;
+  private double suspicionLevel = 0.0;
+  private double maximumSuspicionLevel = 5.0;
 
-  public Suspicion(Rectangle rectangle, SuspicionListener listener) {
+  public Suspicion(Rectangle rectangle, SuspicionListener listener, ProgressBar progressBar) {
     super(rectangle);
+    initialiseIncrementTask();
+    initialiseDecrementTask();
     this.listener = listener;
+    timer = new Timer();
+  }
+
+  private void initialiseIncrementTask() {
+    incrementTask =
+        new TimerTask() {
+          @Override
+          public void run() {
+            suspicionLevel += 0.1;
+          }
+        };
+  }
+
+  private void initialiseDecrementTask() {
+    decrementTask =
+        new TimerTask() {
+          @Override
+          public void run() {
+            suspicionLevel -= 0.1;
+          }
+        };
   }
 
   @Override
@@ -19,11 +51,19 @@ public class Suspicion extends Interactable {
 
   @Override
   public void touched() {
-    listener.suspicionTouched();
+    if (currentTask == decrementTask) {
+      currentTask.cancel();
+      currentTask = incrementTask;
+      timer.scheduleAtFixedRate(currentTask, 0, 100);
+    }
   }
 
   @Override
   public void notTouched() {
-    listener.suspicionUntouched();
+    if (currentTask == incrementTask) {
+      currentTask.cancel();
+      currentTask = decrementTask;
+      timer.scheduleAtFixedRate(currentTask, 0, 100);
+    }
   }
 }
